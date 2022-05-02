5ccad69b7784ed5fc7b26f2034a765cd4452fc2d
Leighiam Virrey <virreyleighiam@gmail.com>
2022-05-01 21:26:01 -0600

    Continuation until exit added

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ def convert_to_morse(text):
     return morse_text
         
         
+running = True
 
+while running:
+    text_to_convert = input("What sentence do you want to convert to morse code? (type exit to quit) ").upper()
+    if text_to_convert == "EXIT":
+        running = False
+        continue
+    usable_text = list(text_to_convert)
 
-text_to_convert = input("What sentence do you want to convert to morse code? ").upper()
-usable_text = list(text_to_convert)
-
-print(f"Here is your translation: {convert_to_morse(usable_text)}")
+    print(f"Here is your translation: {convert_to_morse(usable_text)}")
