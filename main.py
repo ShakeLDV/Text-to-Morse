6980cdfbc16085b121c37b5c6933749c6b4dc88e
Leighiam Virrey <virreyleighiam@gmail.com>
2022-05-01 20:32:04 -0600

    Morse Works

---
+++ main.py
@@ -0,0 +1,13 @@
+from data import morse
+
+def convert_to_morse(text):
+    morse_text = [morse.get(i) for i in text]
+    return " ".join(morse_text)
+        
+        
+
+
+text_to_convert = input("What sentence do you want to convert to morse code? ").upper()
+usable_text = list(text_to_convert)
+
+print(f"Here is your translation: {convert_to_morse(usable_text)}")
